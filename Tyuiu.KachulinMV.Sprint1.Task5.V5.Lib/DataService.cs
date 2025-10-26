c45b4e9537256e218833dde18585fa6805e778e6
A <A@DESKTOP-6S87AHB>
2025-10-26 23:34:33 +0500

    Task5

--- Tyuiu.KachulinMV.Sprint1.Task5.V5.Lib/DataService.cs
+++ Tyuiu.KachulinMV.Sprint1.Task5.V5.Lib/DataService.cs
@@ -1,7 +1,9 @@
 ﻿
+using tyuiu.cources.programming.interfaces.Sprint1;
+
 namespace Tyuiu.KachulinMV.Sprint1.Task5.V5.Lib
 {
-    public class DataService
+    public class DataService: ISprint1Task5V5
     {
         public int Calculate(double x)
         {
